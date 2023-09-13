82ae25b6bdee31da968151d8eb29e6186cd76a4d
hiimbach <lenhobach@gmail.com>
2023-09-13 16:10:28 +0700

    feat(triton/) add triton inferencing

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Number classification with MNIST
 
 This is a side project in Swinburne Vietnam Lab. 
+
+## Triton 
+To run Triton Server:
+`docker run --rm -p 8000:8000 -p 8001:8001 -p 8002:8002 -v ${PWD}/triton:/models nvcr.io/nvidia/tritonserver:23.08-py3 tritonserver --model-repository=/models`
+
+To run Triton Client:
+```
+docker run -it --rm --net=host -v ${PWD}:/workspace/ nvcr.io/nvidia/tritonserver:23.08-py3-sdk bash
+pip install -r requirements
+```
---
+++ triton/inference_server.py
@@ -0,0 +1,38 @@
+import os 
+import sys
+ROOT = os.getcwd()
+if ROOT not in sys.path:
+    sys.path.append(ROOT)
+
+import tritonclient.http as httpclient
+from utils.cv2_to_np import cv2_to_array
+import numpy as np
+import cv2
+
+# Set up connection with Triton Inference Server
+client = httpclient.InferenceServerClient(url="localhost:8000")
+
+# Preprocess Images
+frame = cv2.imread("data/5.1.png", 0)
+arr = cv2_to_array(frame)
+
+# Normalize and convert from FP64 -> FP32
+# norm = (arr-mean)/std
+arr = (arr-0.1307)/0.3081
+
+# Because triton requires shape [B, C, H, W]
+arr = np.expand_dims(arr.astype(np.float32), axis=0) 
+arr = np.expand_dims(arr, axis=0) 
+
+# Names the input and output layers of model
+inputs = httpclient.InferInput("input", arr.shape, datatype="FP32")
+inputs.set_data_from_numpy(arr)
+outputs = httpclient.InferRequestedOutput("output", binary_data=True, class_count=10)
+
+# Querying the server
+results = client.infer(model_name="mnist", inputs=[inputs], outputs=[outputs])
+inference_output = results.as_numpy('output').astype(str)
+
+print(inference_output[0][0][-1])
+# import ipdb; ipdb.set_trace()
+
